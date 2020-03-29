93fbc12264c39a22edc44ff0b439603a9a2d6231
LeviSchouten <levischoutenschool@gmail.com>
2020-03-29 12:09:11 +0200

    started adding handlebars

---
+++ script.js
@@ -0,0 +1,27 @@
+const cast = {
+  characters: [
+    {
+      name: "Michael Scott",
+      title: "Regional Manager"
+    },
+    {
+      name: "Dwight K schrute",
+      title: "Assistant to the Regional Manager"
+    },
+    {
+      name: "Jim Halper",
+      title: "Sales person"
+    },
+    {
+      name: "Pam Beesly",
+      title: "Secretary"
+    }
+  ]
+};
+
+window.onload = event => {
+  const template = document.querySelector("#character-template").textContent;
+  const compiled = Handlebars.compile(template);
+  const list = document.querySelector(".character-list");
+  list.innerHTML = compiled(cast.characters[1]);
+};
