6982f393aed766130db63ce8289447a627b30e1e
crevulus <crevulus@gmail.com>
2020-03-29 15:18:18 +0200

    backend hbs

--- express/server.js
+++ express/server.js
@@ -1,1 +1,32 @@
 const express = require('express');
+const axios = require('axios');
+const app = express();
+
+const handlebars = require('express-handlebars');
+
+const port = 3000;
+
+function getFilmData(res) {
+  axios.get('https://ghibliapi.herokuapp.com/films')
+  .then((data) => res.render('main', {layout: 'index', title: data}));
+}
+
+// const filmData = async () => {
+//   const result = await axios.get('https://ghibliapi.herokuapp.com/films')
+//   .then(response => {
+//     return response.data
+//   });
+// }
+
+app.set('view engine', 'hbs');
+
+app.engine('hbs', handlebars({
+  layoutsDir: __dirname + '/views/layouts',
+  extname: 'hbs'
+}));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => getFilmData(res));
+
+app.listen(port, () => console.log(`App listening to port ${port}`));
